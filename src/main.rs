ae810808c4a8417e1fbeb402866bdc275363e129
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2025-06-19 12:07:20 -0400

    Change port

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 
 fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8080")?;
-    println!("Listening on http://127.0.0.1:8080");
+    let listener = TcpListener::bind("127.0.0.1:8887")?;
+    println!("Listening on http://127.0.0.1:8887");
 
     for stream in listener.incoming() {
         match stream {
